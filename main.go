2d6fba37b202672496dcf58895ca20764fec5396
SiddhiNKabra <nksiddhi20@gmail.com>
2023-08-18 12:07:32 +0530

    search and replace word in the file

--- main.go
+++ main.go
@@ -1,41 +1,57 @@
 package main
 
-import(
+import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
-	"io/ioutil"
 )
 
-func checkerr(err error){
-	if err != nil{
+func check(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
 
-func fileread(filename string){
-	data, err := ioutil.ReadFile(filename)
-	checkerr(err)
-	fmt.Println("Data in the file is\n", string(data))
-}
-
-func create(filename string){
-	file, err := os.Create(filename)
-	checkerr(err)
+func SearchandReplace(filename, search, replace string) error {
+	file, err := os.Open(filename)
+	check(err)
 	defer file.Close()
-	var content string
-	fmt.Println("Enter data to be added to the file: ")
-	fmt.Scanln(&content)
-	trimmedcontent := strings.TrimSpace(content)
-	_, err = file.Write([]byte(trimmedcontent))
-	checkerr(err)
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Replace(scanner.Text(), search, replace, -1)
+		lines = append(lines, line)
+	}
+
+	return ReplaceText(filename, lines)
 }
 
-func main(){
-	fmt.Println("File Handling in Golang")
+func ReplaceText(filename string, lines []string) error {
+	file, err := os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY, 0644)
+	check(err)
+	defer file.Close()
+	for _, line := range lines {
+		_, err := file.WriteString(line + "\n")
+		check(err)
+	}
+	return nil
+}
 
-	create("text.txt")
+func main() {
+	arglen := len(os.Args[1:])
+	if arglen < 3 {
+		fmt.Println("Filename, Search, and Replace needed..")
+		return
+	}
 
-	fileread("./text.txt")
-
+	filename := os.Args[1]
+	search := os.Args[2]
+	replace := os.Args[3]
+	err := SearchandReplace(filename, search, replace)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Search and replace completed successfully.")
+	}
 }
